24e583007f8da159c756773ce117209eb1443e2c
notoir <markn3wton@gmail.com>
2016-11-08 23:35:02 -0500

    done!!!

--- app/src/main/java/edu/umd/mark/doodle/DoodleView.java
+++ app/src/main/java/edu/umd/mark/doodle/DoodleView.java
@@ -17,8 +17,31 @@ import java.util.ArrayList;
 
 public class DoodleView extends View {
 
-    private Paint _paintDoodle = new Paint();
-    private ArrayList<Path> _paths;
+    private Paint _currentPaint = new Paint();
+    private Path _currentPath = new Path();
+
+    public static Paint getCopy(Paint original) {
+        Paint newPaint = new Paint();
+        newPaint.setColor(original.getColor());
+        newPaint.setAntiAlias(original.isAntiAlias());
+        newPaint.setStyle(original.getStyle());
+        newPaint.setStrokeWidth(original.getStrokeWidth());
+        newPaint.setAlpha(original.getAlpha());
+
+        return newPaint;
+    }
+
+    public class PaintPath {
+        public Path _path;
+        public Paint _paint;
+
+        public PaintPath(Path path, Paint paint) {
+            this._path = path;
+            this._paint = paint;
+        }
+    }
+
+    private ArrayList<PaintPath> brushStrokes;
 
     public DoodleView(Context context) {
         super(context);
@@ -36,31 +59,37 @@ public class DoodleView extends View {
     }
 
     private void init(AttributeSet attrs, int defStyle) {
-        _paintDoodle.setColor(Color.RED);
-        _paintDoodle.setAntiAlias(true);
-        _paintDoodle.setStyle(Paint.Style.STROKE);
+        _currentPaint.setColor(Color.RED);
+        _currentPaint.setAntiAlias(true);
+        _currentPaint.setStyle(Paint.Style.STROKE);
+        _currentPaint.setStrokeWidth(30);
+        brushStrokes = new ArrayList<PaintPath>();
+
     }
 
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Path p = new Path();
-        _paths.add(p);
-        canvas.drawPath(p, _paintDoodle);
+        for(PaintPath strokes : this.brushStrokes) {
+            canvas.drawPath(strokes._path, strokes._paint);
+        }
+        canvas.drawPath(this._currentPath, this._currentPaint);
     }
 
     public boolean onTouchEvent(MotionEvent motionEvent) {
         float touchX = motionEvent.getX();
         float touchY = motionEvent.getY();
-        Path newestPath = _paths.get(_paths.size()-1);
 
         switch(motionEvent.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                newestPath.moveTo(touchX, touchY);
+                _currentPath = new Path();
+                _currentPath.moveTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_MOVE:
-                newestPath.lineTo(touchX, touchY);
+                _currentPath.lineTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_UP:
+                PaintPath p = new PaintPath(_currentPath, _currentPaint);
+                brushStrokes.add(p);
                 break;
         }
 
@@ -69,16 +98,62 @@ public class DoodleView extends View {
     }
 
     public void clearDoodle() {
-        _paths.clear();
+        brushStrokes.clear();
     }
 
     public void setBrushSize(CharSequence brushSize) {
+        Paint newPaint = getCopy(this._currentPaint);
         if(brushSize.equals("Small")) {
-            _paintDoodle.setStrokeWidth(30);
+            newPaint.setStrokeWidth(30);
         } else if(brushSize.equals("Medium")) {
-            _paintDoodle.setStrokeWidth(60);
+            newPaint.setStrokeWidth(60);
         } else {
-            _paintDoodle.setStrokeWidth(90);
+            newPaint.setStrokeWidth(90);
         }
+        this._currentPaint = newPaint;
+    }
+
+    public void setColor(String color) {
+        Paint newPaint = getCopy(this._currentPaint);
+        if(color.equals("Red")) {
+            newPaint.setColor(Color.RED);
+        } else if(color.equals("Blue")) {
+            newPaint.setColor(Color.BLUE);
+        } else if(color.equals("Yellow")) {
+            newPaint.setColor(Color.YELLOW);
+        } else if(color.equals("Green")) {
+            newPaint.setColor(Color.GREEN);
+        } else if(color.equals("Magenta")) {
+            newPaint.setColor(Color.MAGENTA);
+        } else {
+            newPaint.setColor(Color.BLACK);
+        }
+        this._currentPaint = newPaint;
+    }
+
+    public void setOpacity(String opacity) {
+        Paint newPaint = getCopy(this._currentPaint);
+        int opacityVal = 0;
+        if(opacity.equals("20%")) {
+            opacityVal = 51;
+        } else if(opacity.equals("40%")) {
+            opacityVal = 102;
+        } else if(opacity.equals("60%")) {
+            opacityVal = 153;
+        } else if(opacity.equals("80%")) {
+            opacityVal = 204;
+        } else {
+            opacityVal = 255;
+        }
+        newPaint.setAlpha(opacityVal);
+        this._currentPaint = newPaint;
+        //invalidate();
+    }
+
+    public void undo() {
+        this.brushStrokes.get(this.brushStrokes.size()-1)._path.reset();
+        invalidate();
+        this.brushStrokes.remove(this.brushStrokes.size()-1);
+        invalidate();
     }
 }
--- app/src/main/java/edu/umd/mark/doodle/MainActivity.java
+++ app/src/main/java/edu/umd/mark/doodle/MainActivity.java
@@ -1,19 +1,85 @@
 package edu.umd.mark.doodle;
 
+import android.graphics.Color;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.PopupMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private String[] _colorEntries;
+    private String[] _opacityEntries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        this._colorEntries = new String[] {
+                "Red", "Blue", "Yellow", "Green", "Blue", "Magenta", "Black"
+        };
+        Spinner colorSpinner = (Spinner) findViewById(R.id.colorSpinner);
+        ArrayAdapter<String> colorAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, _colorEntries);
+        colorSpinner.setAdapter(colorAdapter);
+
+        // set up color spinner
+        this._opacityEntries = new String[] {
+                "20%", "40%", "60%", "80%", "100%"
+        };
+        Spinner opacitySpinner = (Spinner) findViewById(R.id.opacitySpinner);
+        ArrayAdapter<String> opacityAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, _opacityEntries);
+        opacitySpinner.setAdapter(opacityAdapter);
+
+        colorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                DoodleView doodle = (DoodleView)findViewById(R.id.doodleView);
+                doodle.setColor(_colorEntries[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        opacitySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                DoodleView doodle = (DoodleView)findViewById(R.id.doodleView);
+                doodle.setOpacity(_opacityEntries[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     @Override
@@ -22,21 +88,56 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    public void clearView(View v) {
-        DoodleView doodle = (DoodleView)findViewById(R.id.doodleView);
-        doodle.clearDoodle();
-        doodle.invalidate();
-    }
-
     public boolean onOptionsItemSelected(MenuItem item) {
-        View menuItemView = findViewById(R.id.miBrush);
-        switch(item.getItemId()) {
+        //View menuItemView = findViewById(R.id.miBrush);
+        switch (item.getItemId()) {
             case R.id.miBrush:
-                chooseBrushSize(menuItemView);
+                View brushItemView = findViewById(R.id.miBrush);
+                chooseBrushSize(brushItemView);
+                return true;
+            case R.id.miClear:
+                View clearItemView = findViewById(R.id.miClear);
+                clearView(clearItemView);
+                return true;
+            case R.id.miUndo:
+                View undoView = findViewById(R.id.miUndo);
+                undo(undoView);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void undo(View v) {
+        PopupMenu undo = new PopupMenu(MainActivity.this, v);
+        undo.inflate(R.menu.undo_popup);
+        //Toast.makeText(MainActivity.this,"Method is called!", Toast.LENGTH_SHORT).show();
+        undo.show();
+
+        undo.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                DoodleView doodle = (DoodleView)findViewById(R.id.doodleView);
+                doodle.undo();
+                Toast.makeText(MainActivity.this, "Last stroke undone.", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    public void clearView(View v) {
+        PopupMenu clear = new PopupMenu(MainActivity.this, v);
+        clear.inflate(R.menu.clear_popup);
+        //Toast.makeText(MainActivity.this,"Method is called!", Toast.LENGTH_SHORT).show();
+        clear.show();
+
+        clear.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                DoodleView doodle = (DoodleView)findViewById(R.id.doodleView);
+                doodle.clearDoodle();
+                Toast.makeText(MainActivity.this, "Drawing cleared.", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
     }
 
     private void chooseBrushSize(View v) {
@@ -48,13 +149,49 @@ public class MainActivity extends AppCompatActivity {
 
         brushSize.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             public boolean onMenuItemClick(MenuItem item) {
-                DoodleView doodle = (DoodleView)findViewById(R.id.doodleView);
+                DoodleView doodle = (DoodleView) findViewById(R.id.doodleView);
                 doodle.setBrushSize(item.getTitle());
-                Toast.makeText(MainActivity.this,"Brush size: " +
-                        item.getTitle(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Brush size: " +
+                        item.getTitle(), Toast.LENGTH_SHORT).show();
                 return true;
             }
         });
 
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
